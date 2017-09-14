47355d67d5d416add3288ea7a837faecd999d838
shumantt <shumantt@mail.ru>
2017-09-14 23:16:44 +0300

    Save command

--- Filterinqer/ViewModels/MainViewModel.cs
+++ Filterinqer/ViewModels/MainViewModel.cs
@@ -5,11 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
+using System.IO;
 
 namespace Filterinqer
 {
     public class MainViewModel : BaseViewModel
     {
+        private const string BAD_IMAGE_FORMAT = "Возможно обработать изображение только форматов .png и .jpg.";
+
         private ICommand _openCommand;
         private ICommand _saveCommand;
         private ICommand _sobelCommand;
@@ -27,7 +30,15 @@ namespace Filterinqer
                     new Command((param) => true, OpenFileProcess));
             } 
         }
-        public ICommand SaveCommand { get => _saveCommand; }
+        public ICommand SaveCommand
+        {
+            get
+            {
+                return _saveCommand ?? (_saveCommand =
+                    new Command((param) => IsImageSelected, SaveProcess));
+            }
+        }
+
         public ICommand SobelCommand { get => _sobelCommand; }
         public ICommand GausCommand { get => _gausCommand; }
 
@@ -61,12 +72,48 @@ namespace Filterinqer
             var filePath = ChooseFile();
             if (string.IsNullOrEmpty(filePath))
                 return;
+
+            SelectedFile = filePath;
+
             var bitmap = new BitmapImage();
             bitmap.BeginInit();
             bitmap.UriSource = new Uri(filePath);
             bitmap.EndInit();
             SelectedImageSource = bitmap;
             IsImageSelected = true;
+        }
+
+        public void SaveProcess()
+        {
+            BitmapEncoder encoder;
+            if (string.Equals(Path.GetExtension(SelectedFile), ".png", StringComparison.InvariantCultureIgnoreCase))
+                encoder = new PngBitmapEncoder();
+            else if (string.Equals(Path.GetExtension(SelectedFile), ".jpg", StringComparison.InvariantCultureIgnoreCase))
+                encoder = new JpegBitmapEncoder();
+            else
+                throw new InvalidOperationException(BAD_IMAGE_FORMAT);
+
+            encoder.Frames.Add(BitmapFrame.Create(SelectedImageSource));
+
+            var savePath = GetSaveFilePath();
+            if (string.IsNullOrEmpty(savePath))
+                return;
+
+            using (var fileStream = new FileStream(savePath, FileMode.OpenOrCreate))
+                encoder.Save(fileStream);
+        }
+
+        private string GetSaveFilePath()
+        {
+            var saveFileDialog = new SaveFileDialog()
+            {
+                FileName = $"{Path.GetFileNameWithoutExtension(SelectedFile)}_filtered{Path.GetExtension(SelectedFile)}",
+                DefaultExt = ".png",
+                Filter = "PNG |*.png"
+            };
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                return saveFileDialog.FileName;
+            return null;
         }
 
         private string ChooseFile()
